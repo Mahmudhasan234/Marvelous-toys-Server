05582fcce4453d202e811053b3e2faab1dc64541
unknown <sumaiya.rrashid@gmail.comm>
2023-05-20 15:02:10 +0200

    get all toys data

--- index.js
+++ index.js
@@ -35,6 +35,18 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     client.connect();
+
+const toysCollection = client.db('marvelousDB').collection('toys')
+
+// get all the data from DB for initial datas
+
+app.get('/alltoys', async (req, res) => {
+
+    const result  = await toysCollection.find().toArray();
+ 
+    res.send(result);
+})
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
